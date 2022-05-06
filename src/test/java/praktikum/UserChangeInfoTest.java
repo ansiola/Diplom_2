d688e2249a57a9e13d01914a1e821f7c08eb1073
ansiola <ansiola@mail.ru>
2022-05-06 17:24:32 +0300

    commit Diplom_2

--- src/test/java/praktikum/UserChangeInfoTest.java
+++ src/test/java/praktikum/UserChangeInfoTest.java
@@ -20,33 +20,6 @@ public class UserChangeInfoTest {
     }
 
     @Test
-    @Description("Изменение данных пользователя. Одинаковый емаил")
-    public void userInfoCantBeChangeWithSameEmailTest (){
-
-        // Создание пользователя
-        userClient.create(user);
-        // Авторизация пользователя
-        ValidatableResponse login = userClient.login(UserCredentials.from(user));
-        // Получение токена пользователя
-        bearerToken = login.extract().path("accessToken");
-        // Получение информации о пользователе
-        ValidatableResponse info = userClient.userInfoChange(bearerToken, UserCredentials.getUserWithEmail(user));
-        // Получение статус кода с тела информации о пользователе
-        int statusCode = info.extract().statusCode();
-        // Получение тела ответа при запросе информации о пользователе
-        boolean getUserInfo = info.extract().path("success");
-        // Получение сообщения об ошибке
-        String errorMessage = info.extract().path("message");
-
-        // Проверка что статус код соответсвует ожиданиям
-        assertThat ("Status code is incorrect", statusCode, equalTo(403));
-        // Проверка что информация о пользователе запросилась
-        assertFalse ("User info change", getUserInfo);
-        // Проверка тела сообщения
-        assertEquals("User with such email already exists",errorMessage);
-    }
-
-    @Test
     @Description("Изменение данных пользователя. Изменение Email")
     public void userInfoCantBeChangeEmailTest (){
 
